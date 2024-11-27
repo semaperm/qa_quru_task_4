a88cd57339f1637c96c7e9f329afac21b76cac45
Sema Butsyk <semakontakt@mail.ru>
2024-11-27 14:04:14 +0500

    Test manual and auto

--- tests/task.py
+++ tests/task.py
@@ -1,16 +1,16 @@
 import os
-from selene import browser, have,be, by
+from selene import browser, have, be, by
 
 def test_forma_auto():
-    browser.element("#firstName").should(be.blank).type('Sema').press_enter()
-    browser.element("#lastName").should(be.blank).type('Semenov').press_enter()
-    browser.element("#userEmail").should(be.blank).type('sema@test.com').press_enter()
+    browser.element("#firstName").should(be.blank).type('Sema')
+    browser.element("#lastName").should(be.blank).type('Semenov')
+    browser.element("#userEmail").should(be.blank).type('sema@test.com')
 
     browser.element(by.text("Male")).click()
     browser.element("#userNumber").should(be.blank).type("0123456789").press_tab()
     browser.element("#dateOfBirthInput").type('01-02-2002').press_enter()
 
-    browser.element("#subjectsInput").type("English").press_enter()
+    browser.element("#subjectsInput").type("English").press_tab()
     browser.element(by.text("Sports")).click()
     browser.element(by.text("Reading")).click()
     browser.element(by.text("Music")).click()
@@ -23,16 +23,24 @@ def test_forma_auto():
     browser.element("#submit").click()
     browser.element('[class="modal-header"]').should(have.text('Thanks for submitting the form'))
 
-    def test_forma_manual():
-        browser.element("#firstName").should(be.blank).type('Sema').press_enter()
-        browser.element("#lastName").should(be.blank).type('Semenov').press_enter()
-        browser.element("#userEmail").should(be.blank).type('sema@test.com').press_enter()
+def test_forma_munual():
+        browser.element("#firstName").should(be.blank).type('Sema')
+        browser.element("#lastName").should(be.blank).type('Semenov')
+        browser.element("#userEmail").should(be.blank).type('sema@test.com')
 
         browser.element(by.text("Male")).click()
-        browser.element("#userNumber").should(be.blank).type("0123456789").press_tab()
-        #browser.element("#dateOfBirthInput").type('01-02-2002').press_enter()
+        browser.element("#userNumber").should(be.blank).type("0123456789")
 
-        browser.element("#subjectsInput").type("English").press_enter()
+        browser.element("#dateOfBirthInput").click()
+        browser.element(by.text("November")).click()
+        browser.element(by.text("July")).click()
+        browser.element(by.text("2024")).click()
+        browser.element(by.text("2003")).click()
+        browser.element(by.text("2024")).click()
+        browser.element(by.text("2003")).click()
+        browser.element(by.text("11")).click()
+
+        browser.element("#subjectsInput").type("English").press_tab()
         browser.element(by.text("Sports")).click()
         browser.element(by.text("Reading")).click()
         browser.element(by.text("Music")).click()
