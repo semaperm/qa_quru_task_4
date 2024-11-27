ec24f59cdbe89005a7e5cefd5587228d92d05edd
Sema Butsyk <semakontakt@mail.ru>
2024-11-27 13:41:34 +0500

    Test manual

--- tests/task.py
+++ tests/task.py
@@ -1,5 +1,5 @@
 import os
-from selene import browser, be, by
+from selene import browser, have,be, by
 
 def test_forma_auto():
     browser.element("#firstName").should(be.blank).type('Sema').press_enter()
@@ -19,7 +19,9 @@ def test_forma_auto():
     browser.element("#currentAddress").type("Perm")
     browser.element("#state").click().element(by.text("NCR")).click()
     browser.element("#city").click().element(by.text("Delhi")).click()
+
     browser.element("#submit").click()
+    browser.element('[class="modal-header"]').should(have.text('Thanks for submitting the form'))
 
     def test_forma_manual():
         browser.element("#firstName").should(be.blank).type('Sema').press_enter()
@@ -39,4 +41,6 @@ def test_forma_auto():
         browser.element("#currentAddress").type("Perm")
         browser.element("#state").click().element(by.text("NCR")).click()
         browser.element("#city").click().element(by.text("Delhi")).click()
+
         browser.element("#submit").click()
+        browser.element('[class="modal-header"]').should(have.text('Thanks for submitting the form'))
