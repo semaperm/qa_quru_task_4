72e7887af025f1e25adeb1a8dea73bb8e5ffc277
Sema Butsyk <semakontakt@mail.ru>
2024-11-28 10:34:01 +0500

    Fix v1

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,8 @@ from selene import browser
 @pytest.fixture(autouse=True)
 def browser_setting():
     browser.open("https://demoqa.com/automation-practice-form")
-
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
 
     yield
 
